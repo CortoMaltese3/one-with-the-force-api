5298cfbd059cb77702c2094b63301e6b6a19b31b
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-11-03 00:02:46 +0200

    feat/Add new serializers.py for all models and attributes

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Character, Film, Starship
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = "__all__"
+
+
+class FilmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = "__all__"
+
+
+class StarshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Starship
+        fields = "__all__"
