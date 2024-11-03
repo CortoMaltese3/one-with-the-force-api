3463d85ca2d6c0b57932fe8f3fc65d3e59643e69
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-11-03 17:28:22 +0200

    chore/Convert script to Command and add fetch_related_model logic

--- api/management/commands/fetch_swapi_data.py
+++ api/management/commands/fetch_swapi_data.py
@@ -1,12 +1,22 @@
 import requests
-
+from django.core.management.base import BaseCommand
 from api.models import Character, Film, Starship
 
 BASE_URL = "https://swapi.dev/api/"
 
 
-def fetch_and_save_characters():
-    """Fetches character data from SWAPI and saves it to the Character model."""
+# Fetch related resource function
+def fetch_related_model(model, url_list):
+    objects = []
+    for url in url_list:
+        resource_id = url.split("/")[-2]
+        obj = model.objects.filter(url__endswith=f"/{resource_id}/").first()
+        if obj:
+            objects.append(obj)
+    return objects
+
+
+def fetch_characters():
     response = requests.get(f"{BASE_URL}people/")
     if response.status_code == 200:
         data = response.json()
@@ -31,8 +41,7 @@ def fetch_and_save_characters():
             )
 
 
-def fetch_and_save_films():
-    """Fetches film data from SWAPI and saves it to the Film model."""
+def fetch_films():
     response = requests.get(f"{BASE_URL}films/")
     if response.status_code == 200:
         data = response.json()
@@ -45,20 +54,22 @@ def fetch_and_save_films():
                     "director": film_data["director"],
                     "producer": film_data["producer"],
                     "release_date": film_data["release_date"],
-                    "characters": film_data["characters"],
                     "planets": film_data["planets"],
-                    "starships": film_data["starships"],
-                    "vehicles": film_data["vehicles"],
                     "species": film_data["species"],
+                    "vehicles": film_data["vehicles"],
                     "created": film_data["created"],
                     "edited": film_data["edited"],
                     "url": film_data["url"],
                 },
             )
+            # Set character and starship many-to-many fields after creation
+            characters = fetch_related_model(Character, film_data["characters"])
+            starships = fetch_related_model(Starship, film_data["starships"])
+            film.characters.set(characters)
+            film.starships.set(starships)
 
 
-def fetch_and_save_starships():
-    """Fetches starship data from SWAPI and saves it to the Starship model."""
+def fetch_starships():
     response = requests.get(f"{BASE_URL}starships/")
     if response.status_code == 200:
         data = response.json()
@@ -84,3 +95,18 @@ def fetch_and_save_starships():
                     "url": starship_data["url"],
                 },
             )
+            # Set pilot many-to-many field after creation
+            pilots = fetch_related_model(Character, starship_data["pilots"])
+            starship.pilots.set(pilots)
+
+
+class Command(BaseCommand):
+    help = "Fetch and store data from SWAPI"
+
+    def handle(self, *args, **kwargs):
+        fetch_characters()
+        fetch_films()
+        fetch_starships()
+        self.stdout.write(
+            self.style.SUCCESS("Successfully fetched and stored data from SWAPI")
+        )
