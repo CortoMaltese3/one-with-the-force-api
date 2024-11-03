c6cfba4c44bca35e12f3caf641a2d91fa8c3c8d2
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-11-04 00:52:14 +0200

    chore/Adjust celery to run once a day

--- core/settings.py
+++ core/settings.py
@@ -89,8 +89,7 @@ CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_BEAT_SCHEDULE = {
     "fetch-swapi-data-every-day": {
         "task": "api.tasks.fetch_swapi_data_periodically",
-        "schedule": 60,  # every 24 hours
-        # "schedule": 86400,  # every 24 hours
+        "schedule": 86400,  # every 24 hours
     },
 }
 
