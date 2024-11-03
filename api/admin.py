51cd52dd35f329955d5dbaada1f3c75b7170b2c3
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-11-03 17:22:25 +0200

    feat/Register models to the admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Film, Character, Starship
 
-# Register your models here.
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('title', 'episode_id', 'release_date', 'director')
+    search_fields = ('title', 'director', 'producer')
+    list_filter = ('release_date', 'director')
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'birth_year', 'homeworld')
+    search_fields = ('name', 'gender', 'eye_color')
+    list_filter = ('gender', 'eye_color', 'hair_color')
+
+@admin.register(Starship)
+class StarshipAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'manufacturer', 'starship_class')
+    search_fields = ('name', 'model', 'manufacturer')
+    list_filter = ('manufacturer', 'starship_class')
